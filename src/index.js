093fd12b0623921ae1fe85ba8c9ec48527b25ea7
Julien Fontanet <julien.fontanet@isonoe.net>
2016-08-17 12:40:32 +0200

    feat(Xapi#_watchEvents): keep-alive, inject 1 event per minute

--- src/index.js
+++ src/index.js
@@ -13,8 +13,7 @@ import { EventEmitter } from 'events'
 import {
   catchPlus as pCatch,
   delay as pDelay,
-  promisify,
-  timeout as pTimeout
+  promisify
 } from 'promise-toolbox'
 import {
   createClient as createXmlRpcClient,
@@ -611,13 +610,25 @@ export class Xapi extends EventEmitter {
   }
 
   _watchEvents () {
-    const call = () => this._sessionCall('event.from', [
-      ['*'],
-      this._fromToken,
-      1e3 + 0.1 // Force float.
-    ])::pTimeout(600000, call)
+    const injectEvent = () => {
+      const { pool } = this
+      if (pool) {
+        this._sessionCall('event.inject', [ 'pool', pool.$ref ]).catch(console.error)
+      }
+    }
 
-    const loop = () => call().then(onSuccess, onFailure)
+    let timeout
+    const loop = () => {
+      // Keep alive: force at least one event per minute.
+      clearTimeout(timeout)
+      timeout = setTimeout(injectEvent, 60e3)
+
+      return this._sessionCall('event.from', [
+        ['*'],
+        this._fromToken,
+        1e3 + 0.1 // Force float.
+      ]).then(onSuccess, onFailure)
+    }
 
     const onSuccess = ({token, events}) => {
       this._fromToken = token
